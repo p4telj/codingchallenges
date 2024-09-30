3577e695871a53627b6714f63cb5bb554fcf4d78
Jimil Patel <patell.jimil@gmail.com>
2024-09-29 23:44:50 -0400

    feat(main): convert to top-level async for async stream processing

--- ccwc/src/index.ts
+++ ccwc/src/index.ts
@@ -2,14 +2,14 @@
 
 import process from "process";
 import { validate } from "./validate.js";
+import { analyze } from "./analyze.js";
 
-export default function main() {
+export default async function main() {
   const args = process.argv.slice(2);
   const opts = args.slice(0, args.length - 1);
   const file = args[args.length - 1];
 
-  const input = validate(opts, file);
-  console.log("input", input);
+  await analyze(validate(opts, file), file);
 }
 
-main();
+await main();
